49ea7c1207de24710571f5cf9b69957ef172f82d
王强 <wy13708@126.com>
2020-12-06 17:47:56 +0800

    游戏服务器完成

--- day05/application/utils.py
+++ day05/application/utils.py
@@ -4,6 +4,7 @@ def create_http_response(status, response_body):
     response_line = "HTTP/1.1 %s\r\n" % status
     # 9.2 响应头
     response_header = "Server: Python20WS/1.1\r\n"
+    response_header += "Content-Type: text/html\r\n"
     # 9.3 响应空行
     response_blank = "\r\n"
     # 9.4 响应主体
